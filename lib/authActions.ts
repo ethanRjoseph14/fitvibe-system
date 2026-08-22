7e892ed908b5b56387c6e468a98f6e39b429df41
ethanRjoseph14 <ethan@fitvibe.my>
2026-08-22 17:37:53 +0800

    Add memberLogout and adminLogout functions

--- lib/authActions.ts
+++ lib/authActions.ts
@@ -1,1 +1,17 @@
+"use server";
 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { MEMBER_COOKIE, ADMIN_COOKIE } from "@/lib/session";
+
+export async function memberLogout() {
+  const cookieStore = await cookies();
+  cookieStore.delete(MEMBER_COOKIE);
+  redirect("/member/login");
+}
+
+export async function adminLogout() {
+  const cookieStore = await cookies();
+  cookieStore.delete(ADMIN_COOKIE);
+  redirect("/admin/login");
+}
